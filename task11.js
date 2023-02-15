3dd3bdbb9d45ebb0147c0d08452642804624ad55
Denis <denniskost1n@yandex.ru>
2023-02-15 18:14:29 +0300

    task 11 done

---
+++ task11.js
@@ -0,0 +1,19 @@
+const block = document.querySelector('.block')
+const block2 = document.querySelector('.block2')
+const block3 = document.querySelector('.block3')
+
+block.addEventListener("click",() =>{
+
+    block.classList.toggle("block-final")
+
+});
+block2.addEventListener("click",() =>{
+
+    block2.classList.toggle("block-final")
+
+});
+block3.addEventListener("click",() =>{
+
+    block3.classList.toggle("block-final")
+
+});
